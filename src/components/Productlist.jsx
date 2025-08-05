55afae9ced107d42ab7752186ac5dbe6954c93c6
Aleksandergreg <alek3691@stud.kea.dk>
2025-08-05 11:16:16 +0200

    Edited Productlist.jsx to use the correct redux productSlice

--- src/components/Productlist.jsx
+++ src/components/Productlist.jsx
@@ -2,14 +2,14 @@
 import React, { useEffect, useState } from 'react';
 import Product from './Product';
 import { useDispatch, useSelector } from 'react-redux';
-import { setProducts } from '../redux/actions/productActions';
+import { setProducts } from '../redux/productSlice'; // RETTET HER
 import axios from 'axios';
 import { Grid } from '@mui/material';
 
-const Productlist = () => { 
+const Productlist = () => {
     const products = useSelector((state) => state.allProducts.products);
     const dispatch = useDispatch();
-    
+
     const fetchProducts = async () => {
         const response = await axios.get('https://fakestoreapi.com/products')
         .catch((err) => {
@@ -17,16 +17,18 @@ const Productlist = () => {
         });
         dispatch(setProducts(response.data));
     };
-    
+
     useEffect(() => {
         fetchProducts();
     }, []);
-    
+
     return (
         <Grid container spacing={2}>
         {products.map((product) => (
             <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
-            <Product product={product} />
+            {/* The Product component is not provided, so this will need to be created */}
+            {/* <Product product={product} /> */}
+            <pre>{JSON.stringify(product, null, 2)}</pre>
             </Grid>
         ))}
         </Grid>
